125966064c8a96124b71528dc306aa379b4561f0
zaayn <muzayyin937@gmail.com>
2025-11-28 18:42:36 +0700

    integrated with vue

---
+++ resources/js/components/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <h1>Hello from Vue in Laravel 👋</h1>
+      <p>{{ message }}</p>
+    </div>
+  </template>
+  
+  <script setup>
+  const message = 'Ini sudah terintegrasi Laravel + Vue';
+  </script>
+  
+  <style scoped>
+  h1 {
+    font-weight: 600;
+  }
+  </style>
+  
